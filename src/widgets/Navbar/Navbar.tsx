26259327c8325568567a7af66f26751e68424be6
perfilev-danil <danil.perfiliv@gmail.com>
2026-02-27 13:54:31 +0300

    mobile nav

--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,32 +1,84 @@
 import styles from "./Navbar.module.scss";
 import { NavLink } from "react-router-dom";
 import Favorites from "../Favorites/Favorites";
-import { routes } from "../../app/providers/router/routes";
+import { links } from "./links";
 import { getNavLinkClass } from "../../entities/cocktail/lib/utils/nav/nav";
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
-  const links = [
-    { path: routes.Categories, label: "Categories" },
-    { path: routes.Ingredients, label: "Ingredients" },
-    { path: routes.Alcoholic, label: "Alcoholic" },
-    { path: routes.Glass, label: "Glass" },
-  ];
+  const [isOpen, setOpen] = useState(false);
+
   const toActive = getNavLinkClass(styles.navbar__active);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 800) setOpen(false);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navbar__list}>
-        {links.map(({ path, label }) => (
-          <li key={path}>
-            <NavLink to={path} className={toActive}>
-              {label}
-            </NavLink>
+    <>
+      <nav className={styles.navbar}>
+        {isOpen && (
+          <div className={styles.navbar__overlay} onClick={handleClose}></div>
+        )}
+        <ul
+          className={`${styles.navbar__list} ${isOpen ? styles["navbar__list--opened"] : ""}`}
+        >
+          {links.map(({ path, label }) => (
+            <li key={path}>
+              <NavLink className={toActive} to={path} onClick={handleClose}>
+                {label}
+              </NavLink>
+            </li>
+          ))}
+          <li>
+            <Favorites />
           </li>
-        ))}
-        <li>
-          <Favorites />
-        </li>
-      </ul>
-    </nav>
+          {isOpen && (
+            <button onClick={handleClose} aria-label="Close menu">
+              ×
+            </button>
+          )}
+        </ul>
+        <button
+          className={styles.navbar__burger}
+          onClick={handleOpen}
+          aria-label="Toggle navigation menu"
+        >
+          ☰
+        </button>
+      </nav>
+    </>
   );
 };
 
 export default Navbar;
+
+/*
+
+<>
+      <nav className={styles.navbar}>
+        {!isOpen && navList}
+        <button
+          className={styles.navbar__burger}
+          onClick={toggleOpen}
+          aria-label="Toggle navigation menu"
+        >
+          =
+        </button>
+        {isOpen &&
+          createPortal(
+            <div className={styles.wrapper} onClick={toggleOpen}>
+              {navList}
+            </div>,
+            document.body,
+          )}
+      </nav>
+    </>
+
+*/
---
+++ src/widgets/Navbar/links.ts
@@ -0,0 +1,8 @@
+import { routes } from "../../app/providers/router/routes";
+
+export const links = [
+  { path: routes.Categories, label: "Categories" },
+  { path: routes.Ingredients, label: "Ingredients" },
+  { path: routes.Alcoholic, label: "Alcoholic" },
+  { path: routes.Glass, label: "Glass" },
+];
