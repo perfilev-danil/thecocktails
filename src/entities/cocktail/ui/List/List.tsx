ca6e197e95f6df0069da169e7b08332d0f462eca
perfilev-danil <danil.perfiliv@gmail.com>
2026-02-28 14:08:27 +0300

    list & scroll

--- src/entities/cocktail/ui/List/List.tsx
+++ src/entities/cocktail/ui/List/List.tsx
@@ -1,13 +1,30 @@
 import styles from "./List.module.scss";
 import type { CocktailShortUI } from "../../types/ui/cocktails/cocktailShortUI";
 import Card from "../Card/Card";
+import { useLayoutEffect, useRef, useState } from "react";
 
 const List = ({ items }: { items: CocktailShortUI[] }) => {
+  const listRef = useRef<HTMLUListElement>(null);
+  const [isScrollable, setIsScrollable] = useState(false);
+  useLayoutEffect(() => {
+    const currentList = listRef.current;
+    if (!currentList) return;
+    const check = () => {
+      setIsScrollable(currentList.scrollHeight > currentList.clientHeight);
+    };
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, [items]);
+
   if (!items) {
     return <div>no items!</div>;
   }
   return (
-    <ul className={styles.list}>
+    <ul
+      ref={listRef}
+      className={`${styles.list} ${isScrollable ? styles["list--extended"] : ""}`}
+    >
       {items.map((item: CocktailShortUI) => (
         <li className={styles.list__item} key={item.id}>
           <Card item={item} />
