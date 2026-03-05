59973176cc6a466e8811e693750555d729970255
perfilev-danil <danil.perfiliv@gmail.com>
2026-03-05 11:45:39 +0300

    animation apply

---
+++ src/pages/Home/Animation/Animation.tsx
@@ -0,0 +1,12 @@
+import styles from "./Animation.module.scss";
+
+const Animation = () => {
+  return (
+    <div className={styles.animation}>
+      <h2 className={styles.animation__title}>Discover the Art of Cocktails</h2>
+      <p className={styles.animation__subtitle}>Shake. Mix. Enjoy.</p>
+    </div>
+  );
+};
+
+export default Animation;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import styles from "./Home.module.scss";
+import Animation from "./Animation/Animation";
 
 const Home = () => {
-  return <section className={styles.home}></section>;
+  return (
+    <section className={styles.home}>
+      <Animation />
+    </section>
+  );
 };
 
 export default Home;
